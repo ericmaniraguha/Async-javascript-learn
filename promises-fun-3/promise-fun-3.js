0afd0159f0ca03674b5d598b6ce130033fed4206
Eric Maniraguha <ericmaniraguha@gmail.com>
2022-02-10 07:24:40 +0200

    new async-javascript

---
+++ promises-fun-3/promise-fun-3.js
@@ -0,0 +1,26 @@
+
+const getTodos =(resource, callback) =>{
+
+
+   return new Promise ((resolve, reject) =>{
+      const request = new XMLHttpRequest();
+
+      request.addEventListener('readystatechange',()=>{
+       if(request.readyState === 4 && request.status === 200){
+          const data = JSON.parse(request.responseText);
+         
+         resolve(data)
+       }else if(request.readyState === 4){
+           reject('error getting resource')            
+       }
+     }) ;
+       request.open('GET', resource);
+       request.send();
+
+   });
+   };
+getTodos('todos/carla.json').then(data =>{
+   console.log('Promise reolved: ', data);
+}).catch(err =>{
+   console.log("promise rejected: ",err);
+})
